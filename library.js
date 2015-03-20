19fd901dea353092418a7d960aee10560ff932b2
Nikki Evelyn <nikki.evelyn1@gmail.com>
2015-03-20 09:43:29 -0400

    Setting up objects and functions

--- library.js
+++ library.js
@@ -1,5 +1,4 @@
-/*You should initialize your project as a git project, and make small, descriptive commits as you work. When you are finished, please send me the Github link to your completed project.
-
+/*
 The Library
 
 You've been contracted to write a piece of software for the local library. The software needs to meet the following requirements:
@@ -18,5 +17,27 @@ BONUS: This is a library, so books can be checked in and checked out. Librarians
 1.) Change the checked-in/checked-out status of a copy of a book
 2.) Track the name of the person who has checked out a given book and the date when it is due
 3.) Mark a book as overdue
-4.) View a list of checked-in books only*/
+4.) View a list of checked-in books only
+
+Library Object(will hold everything)
+Book Object (will have key value pairs of all the books properties)
+Inventory Object (will have key value pairs of Author: Name of Author & Title: Name of Book)
+Add, Remove & Display will all be methods of Inventory
+Search by title and author (figure out how to set that up)
+*/
+
+var libary = {
+
+}
+var inventory = {
+
+}
+function Book(title, author, genre, length, status){
+	this.title = title;
+	this.author = author;
+	this.genre = genre;
+	this.length = length;
+	this.status = status;
+
+}
 
