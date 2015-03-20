8435589ab2946e1e5a33a4cd4ddd47e3f2b3a237
Nikki Evelyn <nikki.evelyn1@gmail.com>
2015-03-20 09:27:53 -0400

    Setting up sublime file

---
+++ library.js
@@ -0,0 +1,22 @@
+/*You should initialize your project as a git project, and make small, descriptive commits as you work. When you are finished, please send me the Github link to your completed project.
+
+The Library
+
+You've been contracted to write a piece of software for the local library. The software needs to meet the following requirements:
+
+- The software should track the library's inventory of books
+- Each book listing should include the title, author, genre (Fiction, Non-Fiction, etc.), length, and checked-in/checked-out status
+- Librarians should be able to:
+a.) add books to the inventory
+b.) remove books from the inventory 
+c.) view a list of all the books in the library
+d.) view a list of all the books in a given genre
+e.) search for a book by title or author
+
+BONUS: This is a library, so books can be checked in and checked out. Librarians should also be able to:
+
+1.) Change the checked-in/checked-out status of a copy of a book
+2.) Track the name of the person who has checked out a given book and the date when it is due
+3.) Mark a book as overdue
+4.) View a list of checked-in books only*/
+
